1c910960d7485482c6254f632cf2ad93b131c34b
André Luan <andreluan176@gmail.com>
2021-06-03 15:26:42 -0300

    Titulo dinamico

--- src/app/shared/list-title/list-title.component.ts
+++ src/app/shared/list-title/list-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-title',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-title.component.scss']
 })
 export class ListTitleComponent implements OnInit {
+
+  @Input() title: string | undefined;
 
   constructor() { }
 
